cb4a11a57c41cdd4e4121bff03eff919e029b91c
hublogbackenddeveloper <hublogbackenddeveloper@gmail.com>
2025-08-09 12:39:21 +0530

    Minor change

--- models/LeadModel.js
+++ models/LeadModel.js
@@ -176,35 +176,65 @@ const LeadModel = {
     try {
       const queryParams = [];
       let getQuery = `SELECT
-                        l.id,
-                        l.user_id,
-                        l.name,
-                        l.phone_code,
-                        l.phone,
-                        l.whatsapp,
-                        l.email,
-                        l.country,
-                        l.state,
-                        l.district,
-                        l.primary_course_id,
-                        l.primary_fees,
-                        l.price_category,
-                        l.secondary_course_id,
-                        l.secondary_fees,
-                        l.training_mode_id,
-                        l.priority_id,
-                        l.lead_type_id,
-                        l.lead_status_id,
-                        l.response_status_id,
-                        l.next_follow_up_date,
-                        l.expected_join_date,
-                        l.lead_quality_rating,
-                        l.branch_id,
-                        l.batch_track_id,
-                        l.comments,
-                        l.created_date
-                    FROM
-                        lead_master AS l WHERE 1 = 1`;
+                      l.id,
+                      l.user_id,
+                      u.user_name,
+                      l.name,
+                      l.phone_code,
+                      l.phone,
+                      l.whatsapp,
+                      l.email,
+                      l.country,
+                      l.state,
+                      l.district,
+                      l.primary_course_id,
+                      pt.name AS primary_course,
+                      l.primary_fees,
+                      l.price_category,
+                      l.secondary_course_id,
+                      st.name AS secondary_course,
+                      l.secondary_fees,
+                      l.training_mode_id,
+                      tm.name AS training_mode,
+                      l.priority_id,
+                      p.name AS priority,
+                      l.lead_type_id,
+                      lt.name AS lead_type,
+                      l.lead_status_id,
+                      ls.name lead_status,
+                      l.response_status_id,
+                      rs.name AS response_status,
+                      l.next_follow_up_date,
+                      l.expected_join_date,
+                      l.lead_quality_rating,
+                      l.branch_id,
+                      b.name AS branche_name,
+                      l.batch_track_id,
+                      bt.name AS batch_track,
+                      l.comments,
+                      l.created_date
+                  FROM
+                      lead_master AS l
+                  LEFT JOIN users AS u ON
+                    u.id = l.user_id
+                  LEFT JOIN technologies AS pt ON
+                    pt.id = l.primary_course_id
+                  LEFT JOIN technologies AS st ON
+                    st.id = l.secondary_course_id
+                  LEFT JOIN training_mode AS tm ON
+                    l.training_mode_id = tm.id
+                  LEFT JOIN priority AS p ON
+                    p.id = l.priority_id
+                  LEFT JOIN lead_type AS lt ON
+                    lt.id = l.lead_type_id
+                  LEFT JOIN lead_status AS ls ON
+                    ls.id = l.lead_status_id
+                  LEFT JOIN response_status AS rs ON
+                    rs.id = l.response_status_id
+                  LEFT JOIN branches AS b ON
+                    b.id = l.branch_id
+                  LEFT JOIN batch_track AS bt ON
+                    bt.id = l.batch_track_id`;
       if (name) {
         getQuery += ` AND l.name LIKE '%${name}%'`;
       }
@@ -215,8 +245,6 @@ const LeadModel = {
       }
 
       getQuery += ` ORDER BY l.created_date ASC`;
-
-      console.log("Query", getQuery);
 
       const [result] = await pool.query(getQuery, queryParams);
       return result;
