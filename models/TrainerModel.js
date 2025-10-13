940014addc8b3537ae0c8e8ee14b90a6a81d381c
hublogbackenddeveloper <hublogbackenddeveloper@gmail.com>
2025-10-13 17:56:33 +0530

    Minor change

--- models/TrainerModel.js
+++ models/TrainerModel.js
@@ -63,9 +63,12 @@ const TrainerModel = {
         throw new Error("Email or mobile number already exists");
       }
 
-      const [trainer_code] = await pool.query(
-        `SELECT CONCAT('TR', IFNULL(MAX(CAST(SUBSTRING(trainer_id, 3) AS UNSIGNED)), 0) + 1) AS next_trainer_id FROM trainer`
-      );
+      const [trainer_code] = await pool.query(`SELECT 
+                              CONCAT('TR', IFNULL(MAX(CAST(SUBSTRING(trainer_id, 3) AS UNSIGNED)), 0) + 1) AS next_trainer_id
+                          FROM trainer WHERE trainer_id IS NOT NULL 
+                          ORDER BY CAST(SUBSTRING(trainer_id, 3) AS UNSIGNED) DESC
+                          LIMIT 1;
+                          `);
 
       let newId;
       if (trainer_code.length === 0) {
