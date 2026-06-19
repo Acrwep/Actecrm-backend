4f8d354b1c79420daa8492559794911eb5e05c53
hublogbackenddeveloper <hublogbackenddeveloper@gmail.com>
2026-06-19 17:32:43 +0530

    Minor change.

--- models/LeadModel.js
+++ models/LeadModel.js
@@ -1608,7 +1608,7 @@ const LeadModel = {
         [lead_id],
       );
 
-      if (followupCount[0].count <= 0 && !communication_status) {
+      if (followupCount[0].count <= 0 && communication_status != null) {
         let leadStatusId;
         let leadStatusName;
         let leadTemperatureDate;
