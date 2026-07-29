beb8808e44a75481bc436d12c15c23fdc4b51a84
hublogbackenddeveloper <hublogbackenddeveloper@gmail.com>
2026-07-29 17:24:53 +0530

    Get leads API change.

--- models/LeadModel.js
+++ models/LeadModel.js
@@ -4193,7 +4193,7 @@ WHERE ${filterCondition}`;
           IFNULL(SUM(CASE WHEN (ls.name != 'Dormant' OR l.lead_status_id IS NULL) AND (cm1.name IS NULL OR cm1.name NOT IN ('Data Incorrect', 'Data Correct But No Response')) AND ${dateFilterAll} AND l.primary_course_id IS NOT NULL AND l.lead_type_id IS NOT NULL AND (cm.name = 'Not Communicated' OR cm.name IS NULL) THEN 1 ELSE 0 END), 0) as not_communicated_eligible_leads,
           IFNULL(SUM(CASE WHEN (ls.name != 'Dormant' OR l.lead_status_id IS NULL) AND (cm1.name IS NULL OR cm1.name NOT IN ('Data Incorrect', 'Incorrect Data')) AND ${dateFilterAll} AND l.primary_course_id IS NOT NULL AND l.lead_type_id IS NOT NULL AND (cm.name = 'Not Communicated' OR cm.name IS NULL) AND cm1.name = 'Data Correct But No Response' THEN 1 ELSE 0 END), 0) as no_response_eligible_leads,
           IFNULL(SUM(CASE WHEN lh.id IS NOT NULL AND ${dateFilterAll} THEN 1 ELSE 0 END), 0) as interested_leads,
-          IFNULL(SUM(CASE WHEN ${dateFilterAll} THEN 1 ELSE 0 END), 0) as joinings,
+          IFNULL(SUM(CASE WHEN c.id IS NOT NULL AND ${dateFilterAll} THEN 1 ELSE 0 END), 0) as joinings,
           IFNULL(SUM(CASE WHEN lh.id IS NOT NULL AND ls.name = 'Super Hot' AND ${dateFilterAll} THEN 1 ELSE 0 END), 0) as super_hot,
           IFNULL(SUM(CASE WHEN lh.id IS NOT NULL AND ls.name = 'Hot' AND ${dateFilterAll} THEN 1 ELSE 0 END), 0) as hot,
           IFNULL(SUM(CASE WHEN lh.id IS NOT NULL AND ls.name = 'Warm' AND ${dateFilterAll} THEN 1 ELSE 0 END), 0) as warm,
@@ -4271,6 +4271,11 @@ WHERE ${filterCondition}`;
         if (bucket === "Followup Leads") {
           getQuery += ` AND lh.is_updated = 0 AND c.id IS NULL`;
           countQuery += ` AND lh.is_updated = 0 AND c.id IS NULL`;
+        }
+
+        if (bucket === "Interested Leads") {
+          getQuery += ` AND lh.id IS NOT NULL`;
+          countQuery += ` AND lh.id IS NOT NULL`;
         }
 
         if (bucket === "Joinings") {
